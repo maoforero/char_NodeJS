2c528857e4bc917c2ab5f0c38ab9f18f10e8a701
maoforero <maforeror@gmail.com>
2021-12-09 00:38:13 -0500

    ajustando sockets

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+let {Server: SocketIO} = require("socket.io");
+let path = require("path");
+const PORT = 3000;
+
+//Import HTTP para funcionamiento de modulos
+const { Server: HttpServer } = require('http');
+const { Server: IOServer } = require('socket.io');
+
+//Inicializacion del server
+const app = express();
+
+//Paso a una instacia  httpServer a nuestro Servidor
+const httpServer = new HttpServer(app);
+
+// Pasamos nuestra instacia a SocketIO
+const io = new IOServer(httpServer);
+
+//settings
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static("./public"));
+
+//EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", ejs);
+
+app.get("/", (req, res, next) => {
+    res.sendFile('index', {});
+});
+
+//Inicializar funcion express
+httpServer.listen(PORT, () => {
+    console.log("It's Alive 👻");
+});
